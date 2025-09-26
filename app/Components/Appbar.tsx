12edec242600fe0ddc59ff5d3aab3590a2391c6b
akshxdevs <akshxdevs@gmail.com>
2025-09-26 17:07:47 +0530

    create: overall packages added and made changes

--- app/Components/Appbar.tsx
+++ app/Components/Appbar.tsx
@@ -1,15 +1,22 @@
 import { SiWeb3Dotjs } from "react-icons/si";
+import localFont from "next/font/local";
+import WalletConnect from "./WalletConnect";
+import { Wallet } from "lucide-react";
 
+const alone = localFont({
+  src: '../fonts/Ventus/Ventus Italic.otf',
+});
 export const Appbar = () => {
     return (
         <div className="max-w-5xl w-full mx-auto py-4 px-2">
             <div className="flex justify-between items-center">
                 <div className="flex items-center gap-2">
                     <SiWeb3Dotjs size={70} color="red"/>
-                    <h1 className="font-bold text-2xl">E-com DApp</h1>
+                    <h1 className={`font-bold text-2xl`}>E-com DApp</h1>
                 </div>
-                <div>
-                    <button className="bg-purple-700 rounded-md px-4 py-2 text-slate-100 font-semibold">Connect Wallet</button>
+                <div className="flex items-center gap-2">
+                    <Wallet/>
+                    <WalletConnect/>
                 </div>
             </div>
         </div>
