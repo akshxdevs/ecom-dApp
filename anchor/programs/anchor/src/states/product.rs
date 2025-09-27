14cd015b3f367fc286d782ee402047468e39c50e
akshxdevs <akshxdevs@gmail.com>
2025-09-27 22:31:40 +0530

    update: added states

---
+++ anchor/programs/anchor/src/states/product.rs
@@ -0,0 +1,44 @@
+use anchor_lang::prelude::*;
+
+#[account]
+pub struct Product {
+    pub product_id: u32,
+    pub product_name: String,
+    pub category: Category,
+    pub division: Division,
+    pub quantity: u32,
+    pub seller_pubkey: Pubkey,
+    pub seller_name: String,
+    pub product_short_description: String,
+    pub product_imgurl: String,
+    pub price: u32,
+    pub rating: f32,
+    pub stock_status: Stock,
+    pub creation_bump:u8,
+}
+
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
+pub enum Category {
+    Electronics,
+    BeautyAndPersonalCare,
+    SnacksAndDrinks,
+    HouseholdEssentials,
+    GroceryAndKitchen,
+}
+
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
+pub enum Division {
+    Mobile,
+    Laptop,
+    Headphone,
+    SmartWatch,
+    ComputerPeripherals,
+}
+
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
+pub enum Stock {
+    OutOfStock,
+    InStock,
+    Restoring
+}
+
