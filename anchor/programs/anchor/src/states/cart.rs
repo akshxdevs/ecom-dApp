ffb1ae8c3910cde7b1135e331c711bb6d651f6fc
akshxdevs <akshxdevs@gmail.com>
2025-09-29 23:11:23 +0530

    update: added stock_status

--- anchor/programs/anchor/src/states/cart.rs
+++ anchor/programs/anchor/src/states/cart.rs
@@ -7,6 +7,7 @@ pub struct Cart{
     pub quantity: u32,
     pub seller_pubkey: Pubkey,
     pub product_imgurl: String,
+    pub stock_status: Stock,
     pub price: u32,
     pub cart_bump:u8,
 }
@@ -14,4 +15,11 @@ pub struct Cart{
 #[account]
 pub struct Cartlist{
     pub cart_list: Vec<Pubkey>, 
+}
+
+#[derive(AnchorDeserialize,AnchorSerialize,Clone)]
+pub enum Stock {
+    OutOfStock,
+    InStock,
+    Restoring
 }
