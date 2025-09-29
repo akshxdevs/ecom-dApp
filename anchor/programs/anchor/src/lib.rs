68e57e27e77d96923b46f58c6a72a7a98e7d3329
akshxdevs <akshxdevs@gmail.com>
2025-09-29 22:56:03 +0530

    update: added create product, show product list, add to cart and cart list

--- anchor/programs/anchor/src/lib.rs
+++ anchor/programs/anchor/src/lib.rs
@@ -19,15 +19,43 @@ pub mod ecom_dapp {
         price:u32,
         category: Category,
         division:Division,
-        creation_bump:u8,
         seller_name:String,
         product_imgurl:String,    
     ) -> Result<()> {
         ctx.accounts.create_product(
-            product_name, product_short_description, price, category, division, seller_name, product_imgurl, creation_bump
+            product_name, 
+            product_short_description, 
+            price, category, 
+            division, 
+            seller_name, 
+            product_imgurl, 
+            ctx.bumps.product,
         )?;
         let product_key = ctx.accounts.product.key();
         ctx.accounts.product_list.products.push(product_key);
+        Ok(())
+    }
+
+    pub fn add_to_cart(
+        ctx: Context<AddToCart>,
+        product_id: u32,
+        product_name: String,
+        quantity: u32,
+        seller_pubkey: Pubkey,
+        product_imgurl: String,
+        price: u32,
+    ) -> Result<()> {
+        ctx.accounts.add_to_cart(
+            product_id,
+            product_name,
+            quantity,
+            seller_pubkey,
+            product_imgurl,
+            price,
+            ctx.bumps.cart,
+        )?;
+        let cart_key = ctx.accounts.cart.key();
+        ctx.accounts.cart_list.cart_list.push(cart_key);
         Ok(())
     }
 }
