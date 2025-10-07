5fd3625ecc5cdbf8ebafe3c2e16b331300f181dc
akshxdevs <akshxdevs@gmail.com>
2025-10-07 14:01:08 +0530

    update: made changes

--- anchor/programs/anchor/src/instructions/create_product.rs
+++ anchor/programs/anchor/src/instructions/create_product.rs
@@ -1,45 +1,7 @@
 use anchor_lang::prelude::*;
 use crate::states::{Category, Division, Product, ProductCreated, ProductsList, Stock};
-
-
-impl <'info> CreateProduct<'info> {
-    pub fn create_product(
-        &mut self,
-        product_name:String,
-        product_short_description:String,
-        price:u32,
-        category: Category,
-        division:Division,
-        seller_name:String,
-        product_imgurl:String,
-        creation_bump:u8,
-    ) -> Result<()> {
-        self.product.set_inner(Product { 
-            product_id: 0, 
-            product_name:product_name.clone(), 
-            category:category.clone() , 
-            division:division.clone(), 
-            quantity: 100, 
-            seller_pubkey:self.seller.key().clone(), 
-            seller_name, 
-            product_short_description, 
-            product_imgurl, 
-            price, 
-            rating: 0.0, 
-            stock_status: Stock::InStock,
-            creation_bump  
-        });
-        emit!(ProductCreated{
-            product_pubkey:self.product.key(),
-            seller:self.seller.key(),
-            product_name,
-            price,
-            category,
-            division
-        });
-        Ok(())
-    }
-}
+use anchor_lang::solana_program::hash::{self};
+use anchor_lang::solana_program::program_error::ProgramError;
 
 
 #[derive(Accounts)]
@@ -79,3 +41,55 @@ pub struct CreateProduct<'info> {
 
     pub system_program: Program<'info, System>,
 }
+
+impl <'info> CreateProduct<'info> {
+    pub fn create_product(
+        &mut self,
+        product_name:String,
+        product_short_description:String,
+        price:u32,
+        category: Category,
+        division:Division,
+        seller_name:String,
+        product_imgurl:String,
+        creation_bump:u8,
+    ) -> Result<()> {
+        let now = Clock::get()?.unix_timestamp;
+        let seed_data = [
+            self.seller.key().as_ref(),
+            &now.to_le_bytes(),
+        ].concat();
+        let hash = hash::hash(&seed_data);
+        let product_id: [u8; 16]  = hash.to_bytes()[..16]
+            .try_into()
+            .map_err(|_| ProgramError::InvalidInstructionData)?;
+        msg!("Generated Product ID: {:?}", product_id);
+
+        self.product.set_inner(Product { 
+            product_id: product_id, 
+            product_name:product_name.clone(), 
+            category:category.clone() , 
+            division:division.clone(), 
+            quantity: 100, 
+            seller_pubkey:self.seller.key().clone(), 
+            seller_name, 
+            product_short_description, 
+            product_imgurl, 
+            price, 
+            rating: 0.0, 
+            stock_status: Stock::InStock,
+            creation_bump  
+        });
+        emit!(ProductCreated{
+            product_pubkey:self.product.key(),
+            seller:self.seller.key(),
+            product_name,
+            price,
+            category,
+            division
+        });
+        Ok(())
+    }
+}
+
+
