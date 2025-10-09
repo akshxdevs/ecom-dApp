fbe4707480e71057cb206e9d1574fd73727da7c2
akshxdevs <akshxdevs@gmail.com>
2025-10-09 22:40:24 +0530

    update: added add to cart and cart list tests

--- anchor/tests/anchor.ts
+++ anchor/tests/anchor.ts
@@ -13,6 +13,16 @@ describe("anchor", () => {
   const signer = provider.wallet.publicKey;
 
   let seller = Keypair.generate();
+  let consumer = Keypair.generate();
+  
+  let product_id:any;
+  let product_name: string;
+  let product_short_description: string;
+  let price: number;
+  let category: any;
+  let division: any;
+  let seller_name: string;
+  let product_imgurl: string;
 
   it("should initialize and create product!", async () => {
     await provider.connection.requestAirdrop(
@@ -21,13 +31,13 @@ describe("anchor", () => {
     );
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    const product_name = "IPhone 17 Pro";
-    const product_short_description = "The iPhone 17 Pro, launched September 2025, is a premium flagship with a 6.3-inch ProMotion display, A19 Pro chip, and advanced triple-camera system.";
-    const price = 799;
-    const category = { electronics: {} };
-    const division = { mobile: {} };
-    const seller_name = "Apple";
-    const product_imgurl = "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-pro-finish-select-202509-6-3inch-cosmicorange?wid=5120&hei=2880&fmt=webp&qlt=90&.v=NUNzdzNKR0FJbmhKWm5YamRHb05tUzkyK3hWak1ybHhtWDkwUXVINFc0RUlmWnJkM2NiV2hVVVF2ZE1VdGpQZWhsQTdPYWVGbmdIenAvNE9qYmZVYVFDb1F2RTNvUEVHRkpGaGtOSVFHak5NTEhXRE11VU1QNVo2eDJsWlpuWHRlLys5ZkozSXJXZWZXNk8rZDF5S0V3&traceId=1";
+    product_name = "IPhone 17 Pro";
+    product_short_description = "The iPhone 17 Pro, launched September 2025, is a premium flagship with a 6.3-inch ProMotion display, A19 Pro chip, and advanced triple-camera system.";
+    price = 799;
+    category = { electronics: {} };
+    division = { mobile: {} };
+    seller_name = "Apple";
+    product_imgurl = "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-pro-finish-select-202509-6-3inch-cosmicorange?wid=5120&hei=2880&fmt=webp&qlt=90&.v=NUNzdzNKR0FJbmhKWm5YamRHb05tUzkyK3hWak1ybHhtWDkwUXVINFc0RUlmWnJkM2NiV2hVVVF2ZE1VdGpQZWhsQTdPYWVGbmdIenAvNE9qYmZVYVFDb1F2RTNvUEVHRkpGaGtOSVFHak5NTEhXRE11VU1QNVo2eDJsWlpuWHRlLys5ZkozSXJXZWZXNk8rZDF5S0V3&traceId=1";
 
     // Calculate PDAs
     const [productPda, productBump] = PublicKey.findProgramAddressSync(
@@ -57,7 +67,7 @@ describe("anchor", () => {
         product: productPda,
         productList: productListPda,
         systemProgram: SYSTEM_PROGRAM_ID,
-      })
+      } as any)
       .signers([seller])
       .rpc();
 
@@ -77,7 +87,8 @@ describe("anchor", () => {
         hex.slice(20),
       ].join("-");
     }
-    console.log("Product Id:", bytesToUuid(product.productId));
+    product_id = bytesToUuid(product.productId);
+    console.log("Product Id:", product_id);
     const getSellerBalance = await provider.connection.getBalance(
       seller.publicKey
     );
@@ -85,13 +96,13 @@ describe("anchor", () => {
     console.log("Seller Balance: ", seller_balance + "SOL");
   });
   it("creating another product!", async () => {
-    const product_name = "MacBook Pro";
-    const product_short_description = "Powerful, sleek, high-performance laptop with stunning display and long battery.";
-    const price = 1599;
-    const category = { electronics: {} };
-    const division = { laptop: {} };
-    const seller_name = "Apple";
-    const product_imgurl = "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRTYbESY_XvQ5bcb8ukHm5q0LCmvzK9AaK5DyMGbcFqIovxJvYCCkG-5AQU5IqaBI_EIXeqDuY9nGJ7Q8P9P_f_7ZBkuu3l01ucR-iNkEa5OKrHs5L8kF53";
+    product_name = "MacBook Pro";
+    product_short_description = "Powerful, sleek, high-performance laptop with stunning display and long battery.";
+    price = 1599;
+    category = { electronics: {} };
+    division = { laptop: {} };
+    seller_name = "Apple";
+    product_imgurl = "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRTYbESY_XvQ5bcb8ukHm5q0LCmvzK9AaK5DyMGbcFqIovxJvYCCkG-5AQU5IqaBI_EIXeqDuY9nGJ7Q8P9P_f_7ZBkuu3l01ucR-iNkEa5OKrHs5L8kF53";
 
     // Calculate PDAs
     const [productPda, productBump] = PublicKey.findProgramAddressSync(
@@ -121,7 +132,7 @@ describe("anchor", () => {
         product: productPda,
         productList: productListPda,
         systemProgram: SYSTEM_PROGRAM_ID,
-      })
+      } as any)
       .signers([seller])
       .rpc();
 
@@ -141,17 +152,18 @@ describe("anchor", () => {
         hex.slice(20),
       ].join("-");
     }
-    console.log("Product Id:", bytesToUuid(product.productId));
+    product_id = bytesToUuid(product.productId);
+    console.log("Product Id:", product_id);
   });
   it("should display all the created products", async () => {
-    const product_name = "Apple Watch SE";
+    product_name = "Apple Watch SE";
     const product_short_description =
       "The Apple Watch is Apple's premium smartwatch lineup, designed for health tracking, fitness, connectivity, and safety.";
-    const price = 249;
-    const category = { electronics: {} };
-    const division = { smartWatch: {} };
-    const seller_name = "Apple";
-    const product_imgurl =
+    price = 249;
+    category = { electronics: {} };
+    division = { smartWatch: {} };
+    seller_name = "Apple";
+    product_imgurl =
       "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSfoMHgS1tRqOz2hmUMFLJ4z6RqDlr5QqKlJ9_IOuUBMlcASEw1778AlhSNluH43nlADPMe20U7ZkmDoEsx_7w8d0r_6cJjvHKTQtPPR-1r5CrMhqfeDGib7L8";
 
     // Calculate PDAs
@@ -182,7 +194,7 @@ describe("anchor", () => {
         product: productPda,
         productList: productListPda,
         systemProgram: SYSTEM_PROGRAM_ID,
-      })
+      } as any)
       .signers([seller])
       .rpc();
 
@@ -202,7 +214,9 @@ describe("anchor", () => {
         hex.slice(20),
       ].join("-");
     }
-    console.log("Product Id:", bytesToUuid(product.productId));
+    product_id = bytesToUuid(product.productId);
+    console.log("Product Id:", product_id);    
+
     const getSellerBalance = await provider.connection.getBalance(
       seller.publicKey
     );
@@ -214,4 +228,65 @@ describe("anchor", () => {
     );
     console.log("Product List: ", productList);
   });
+
+  it("should add product to cart",async()=>{
+    await provider.connection.requestAirdrop(
+      consumer.publicKey,
+      2* anchor.web3.LAMPORTS_PER_SOL
+    );
+    await new Promise((res)=> setTimeout(res,1000));
+    
+    // Use the actual 16-byte product ID from the created product
+    const [cartPda] = PublicKey.findProgramAddressSync(
+      [Buffer.from("cart"), consumer.publicKey.toBuffer(), Buffer.from(product_id)],
+      program.programId
+    );
+    const [cartListPda] = PublicKey.findProgramAddressSync(
+      [Buffer.from("cart_list"),consumer.publicKey.toBuffer(),],
+      program.programId
+    );
+
+    console.log(product_id);
+    
+    
+    const tx = await program.methods
+      .addToCart(
+        product_id,
+        product_name,        
+        1,                   
+        seller.publicKey,    
+        product_imgurl,      
+        price                
+      )
+      .accounts({
+        consumer: consumer.publicKey,
+        cart: cartPda,
+        cartList: cartListPda,
+        systemProgram: SYSTEM_PROGRAM_ID,
+      })
+      .signers([consumer])
+      .rpc();
+
+    console.log("Your transaction signature", tx);
+    const cart = await program.account.cart.fetch(cartPda);
+    console.log("Cart: ", cart);
+    const cartList = await program.account.cartlist.fetch(cartListPda);
+    console.log("CartList: ", cartList);
+  });
+
+  // it("should add to cart & display cart list of publickey",async()=>{
+
+  // });
+
+  // it("should create a escrow",async()=>{
+
+  // });
+
+  // it("should transfer funds perform payment",async()=>{
+
+  // });
+
+  // it("should book the order",async()=>{
+
+  // });
 });
