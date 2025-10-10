91643a4769f89d3b2ea9673831ee5c2d6e26e71f
akshxdevs <akshxdevs@gmail.com>
2025-10-10 12:58:44 +0530

    update: added add to cart and card list logic

--- anchor/programs/anchor/src/instructions/add_to_cart.rs
+++ anchor/programs/anchor/src/instructions/add_to_cart.rs
@@ -1,34 +1,39 @@
 use anchor_lang::prelude::*;
-use crate::states::cart::{Cart, CartCreated, Cartlist, Stock};
-
+use crate::states::{cart::{Cart, CartCreated, CartList, Stock}, Product};
 #[derive(Accounts)]
-#[instruction(product_id:u32, product_name: String, product_imgurl: String)]
+#[instruction(product_name: String)]
 pub struct AddToCart<'info> {
     #[account(mut)]
     pub consumer: Signer<'info>,
 
     #[account(
-        init,
+        init_if_needed,
         payer = consumer,
-        seeds = [b"cart", consumer.key().as_ref(), &product_id.swap_bytes()],
+        seeds = [
+            b"cart", consumer.key().as_ref(), 
+            product_name.as_bytes()
+        ],
+        bump,
         space = 8 + Cart::INIT_SPACE
     )]
     pub cart: Account<'info, Cart>,
+
+    #[account(mut)]
+    pub products:Account<'info,Product>,
     #[account(
         init_if_needed,
         payer = consumer,
         seeds= [b"cart_list",consumer.key().as_ref()],
         bump,
-        space = 8 + Cartlist::INIT_SPACE
+        space = 8 + CartList::INIT_SPACE
     )]
-    pub cart_list:Account<'info,Cartlist>,
+    pub cart_list:Account<'info,CartList>,
     pub system_program: Program<'info, System>,
 }
 
 impl <'info> AddToCart <'info> {
     pub fn add_to_cart(
         &mut self,
-        product_id: String,
         product_name: String,
         quantity: u32,
         seller_pubkey: Pubkey,
@@ -36,6 +41,7 @@ impl <'info> AddToCart <'info> {
         price: u32,
         cart_bump:u8,
     ) -> Result<()>{
+        let product_id = self.products.product_id;
         self.cart.set_inner(Cart 
             { 
                 product_id, 
@@ -52,21 +58,22 @@ impl <'info> AddToCart <'info> {
             product_name,
             price,
             quantity,
-            seller:self.cart.seller_pubkey,
+            seller: seller_pubkey,
         });
         Ok(())
     }
+    
     pub fn cart_list(
         &mut self,
         cart_list_bump:u8,
     )->Result<()>{
-        if self.cart_list.cart_list.is_empty() && self.cart_list.cart_list_bump == 0 {
-            self.cart_list.set_inner(Cartlist { 
+        // Only initialize if the cart_list is empty (new account)
+        if self.cart_list.cart_list.is_empty() {
+            self.cart_list.set_inner(CartList { 
                 cart_list: Vec::new(), 
                 cart_list_bump 
             });
         }
-
         Ok(())
     }
 }
