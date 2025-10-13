211a4a592dead36d92d5c8c21163f6790be0f712
akshxdevs <akshxdevs@gmail.com>
2025-10-13 22:58:23 +0530

    update: added main changes

--- anchor/programs/anchor/src/states/cart.rs
+++ anchor/programs/anchor/src/states/cart.rs
@@ -6,12 +6,13 @@ pub struct Cart{
     pub product_id: [u8;16],
     #[max_len(50)]
     pub product_name: String,
-    pub quantity: u32,
+    pub quantity: u64,
     pub seller_pubkey: Pubkey,
     #[max_len(150)]
     pub product_imgurl: String,
     pub stock_status: Stock,
-    pub price: u32,
+    #[max_len(40)]
+    pub amount: Vec<u64>,
     pub cart_bump:u8,
 }
 
@@ -19,15 +20,16 @@ pub struct Cart{
 #[derive(InitSpace)]
 pub struct CartList{
     #[max_len(40)]
-    pub cart_list: Vec<Pubkey>, 
+    pub cart_list: Vec<Pubkey>,
+    pub total_amount:u64, 
     pub cart_list_bump:u8,
 }
 #[event]
 pub struct CartCreated {
     pub seller: Pubkey,
-    pub quantity: u32,
+    pub quantity: u64,
     pub product_name: String,
-    pub price: u32,
+    pub amount: u64,
 }
 #[derive(AnchorDeserialize,AnchorSerialize,Clone,InitSpace, PartialEq, Eq)]
 pub enum Stock {
