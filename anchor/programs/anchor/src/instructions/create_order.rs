02524b53d520ae671a38b39c35dea682ea1d36dd
akshxdevs <akshxdevs@gmail.com>
2025-10-14 15:25:00 +0530

    update: made main changes

--- anchor/programs/anchor/src/instructions/create_order.rs
+++ anchor/programs/anchor/src/instructions/create_order.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use crate::states::{cart::Cart, order::{Order, OrderStatus, OrderTracking}, payment::Payment};
+use crate::states::{order::{Order, OrderStatus, OrderTracking}, payment::{Payment}};
 use anchor_lang::solana_program::hash::{self};
 #[derive(Accounts)]
 pub struct CreateOrder<'info>{
@@ -14,19 +14,13 @@ pub struct CreateOrder<'info>{
         space = 9 + Order::INIT_SPACE,
     )]
     pub order:Account<'info,Order>,
-    #[account(mut)]
-    pub cart:Account<'info,Cart>,
-    #[account(mut)]
-    pub payment:Account<'info,Payment>,
     pub system_program:Program<'info,System>,
 }
 
 impl<'info> CreateOrder<'info> {
     pub fn create_order(
         &mut self,
-        product_id:[u8;16],
-        payment_id:[u8;16],
-        tracking_id:[u8;16],
+        payment_id:String,
         order_bump:u8,
     ) -> Result<()> {
         let clock = Clock::get()?;
@@ -35,17 +29,23 @@ impl<'info> CreateOrder<'info> {
             self.signer.key().as_ref(),
             &clock.unix_timestamp.to_le_bytes(),
         ].concat();
+        
         let hash = hash::hash(&seed_data);
+        
         let order_id = hash.to_bytes()[..16]
             .try_into()
             .map_err(|_| ProgramError::InvalidInstructionData)?;
+        
+        let tracking_id = hash.to_bytes()[..16]
+        .try_into()
+        .map_err(|_| ProgramError::InvalidInstructionData)?;
+        
         self.order.set_inner(Order { 
             order_id: order_id,
-            product_id , 
             payment_id, 
-            tracking_id , 
-            order_status:OrderStatus::Pending, 
-            order_tracking:OrderTracking::WatingForOrders, 
+            tracking_id, 
+            order_status:OrderStatus::Placed, 
+            order_tracking:OrderTracking::Booked, 
             created_at:clock.unix_timestamp, 
             updated_at:clock.unix_timestamp, 
             order_bump,
