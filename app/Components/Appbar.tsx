c7e7de97a837d5fb8c7ab40d7e9b75ab548747f4
akshxdevs <akshxdevs@gmail.com>
2025-10-18 22:32:58 +0530

    update: made changes

--- app/Components/Appbar.tsx
+++ app/Components/Appbar.tsx
@@ -1,16 +1,17 @@
+"use client";
 import WalletConnect from "./WalletConnect";
 import {  BellIcon, PlusSquareIcon, SearchIcon, ShoppingBagIcon, Wallet2Icon } from "lucide-react";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { Connection, clusterApiUrl,LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { useEffect, useState } from "react";
-import { useShowCreateModal } from "../createproduct/page";
+import { useRouter } from "next/navigation";
 
 
 export const Appbar = () => {
-    const { publicKey, connected } = useWallet();
+    const { publicKey } = useWallet();
     const connection = new Connection(clusterApiUrl("devnet"));
     const [balance,setBalance] = useState<number>(0.0013);
-    const {showCreateModal, setShowCreateModal} = useShowCreateModal();
+    const router = useRouter();
     useEffect(()=>{
         fetchBalance();
     },[publicKey])
@@ -27,9 +28,9 @@ export const Appbar = () => {
     return (
             <div className="max-w-7xl w-full mx-auto p-2">
                 <div className="flex justify-between items-center gap-16">
-                    <div className="flex items-center gap-1">
+                    <div onClick={()=>router.push("/")} className="flex items-center gap-1">
                         <ShoppingBagIcon size={25}/>
-                        <h1 className={`font-bold text-xl`}>blockBazzar</h1>
+                        <h1 className={`font-bold text-xl cursor-pointer`}>blockBazzar</h1>
                     </div>
                     <div className="w-full flex items-center gap-4">
                         <div className="px-2 flex items-center gap-1 w-[40%] mr-8 border border-transparent outline-none focus:outline-none focus:border-blue-600 bg-[#223351] rounded-md">
@@ -40,7 +41,7 @@ export const Appbar = () => {
                                 className="w-full text-md py-1 outline-none border-none"
                             />
                         </div>
-                        <button onClick={()=>setShowCreateModal(true)} className="flex items-center text-slate-400 gap-2 p-2 text-sm font-semibold border-2 border-blue-900 rounded-lg hover:text-slate-100">
+                        <button onClick={()=>router.push("/createproduct")} className="flex items-center text-slate-400 gap-2 p-2 text-sm font-semibold border-2 border-blue-900 rounded-lg hover:text-slate-100">
                             <PlusSquareIcon size={18} />
                             List a product
                         </button>
