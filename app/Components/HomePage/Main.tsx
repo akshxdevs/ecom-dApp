245e089d40d6315f43701d471fcacd22c8852941
akshxdevs <akshxdevs@gmail.com>
2025-10-21 19:48:36 +0530

    update: made main changes

--- app/Components/HomePage/Main.tsx
+++ app/Components/HomePage/Main.tsx
@@ -1,8 +1,19 @@
+"use client";
+import { ShoppingCart } from "lucide-react"
 import { Products } from "./Products"
+import { useRouter } from "next/navigation"
 
 export const Main = () => {
+    const router = useRouter();
     return <div className="p-16">
-        <h1>Products</h1>
+        <div className="flex justify-between py-5">
+            <div>
+                <h1>Products</h1>
+            </div>
+            <div>
+                <button onClick={()=>router.push("/cart")} className="cursor-pointer"><ShoppingCart/></button>
+            </div>
+        </div>
         <Products/>
     </div>
 }
