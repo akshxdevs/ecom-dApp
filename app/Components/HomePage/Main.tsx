ca33b8df3261cefc079cda1f4e4800d0bfe64824
akshxdevs <akshxdevs@gmail.com>
2025-10-22 16:06:02 +0530

    update: made main changes

--- app/Components/HomePage/Main.tsx
+++ app/Components/HomePage/Main.tsx
@@ -1,19 +1,42 @@
 "use client";
-import { ShoppingCart } from "lucide-react"
-import { Products } from "./Products"
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { fetchCart, fetchCartList } from "@/sdk/program";
+import { PublicKey } from "@solana/web3.js";
+import { Products, useCartLength } from "./Products";
+import { ShoppingCart } from "lucide-react";
+import toast from "react-hot-toast";
+
+interface Cart {
+  productName: string;
+  amount: any;
+  sellerPubkey: string;
+  productImgurl: string;
+  quantity: number;
+}
 
 export const Main = () => {
-    const router = useRouter();
-    return <div className="p-16">
-        <div className="flex justify-between py-5">
-            <div>
-                <h1>Products</h1>
-            </div>
-            <div>
-                <button onClick={()=>router.push("/cart")} className="cursor-pointer"><ShoppingCart/></button>
-            </div>
+  const router = useRouter();
+  const {cartLength} = useCartLength(); 
+
+  return (
+    <div className="p-16">
+      <div className="flex justify-between py-5">
+        <div>
+          <h1>Products</h1>
         </div>
-        <Products/>
+        <div className="text-red-500">
+          <button
+            onClick={() => router.push("/cart")}
+            className="text-white cursor-pointer"
+          >
+            <ShoppingCart />
+          </button>
+          {cartLength}
+        </div>
+      </div>
+      <Products />
     </div>
-}
+  );
+};
